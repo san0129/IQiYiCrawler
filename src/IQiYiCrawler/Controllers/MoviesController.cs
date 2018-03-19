5b0c91661da07a825fa1f7893e6972e2bbc09ba3
未来 <kuangsan666@outlook.com>
2018-03-19 18:15:48 +0800

    bug

--- src/IQiYiCrawler/Controllers/MoviesController.cs
+++ src/IQiYiCrawler/Controllers/MoviesController.cs
@@ -22,9 +22,10 @@ namespace IQiYiCrawler.Controllers
         //    return MoviesSearch.Crawler($"http://list.iqiyi.com/www/{pindao}/{region}-{type}-----{guige}-----{year}--11-{page}-1-iqiyi--.html");
         //}
         [HttpGet]
-        public IEnumerable<MovieViewModel> Get(string classify = "/www/1/-------------11-{0}-1-iqiyi--.html", int page = 1)
+        public IEnumerable<MovieViewModel> Get(int page = 1, string classify = "/www/1/-------------11-{0}-1-iqiyi--.html")
         {
-            if (string.IsNullOrEmpty(classify)) {
+            if (string.IsNullOrEmpty(classify))
+            {
                 classify = "/www/1/-------------11-{0}-1-iqiyi--.html";
             }
             //11-2-1-iqiyi--
@@ -35,13 +36,13 @@ namespace IQiYiCrawler.Controllers
                 if (index > 0)
                 {
                     classify = classify.Remove(index - 1, 1);
-                    classify = classify.Insert(index-1, "{0}");
+                    classify = classify.Insert(index - 1, "{0}");
                 }
                 else
                 {
                     index = classify.IndexOf("iqiyi");
                     classify = classify.Remove(index - 3, 1);
-                    classify = classify.Insert(index -2, "1");
+                    classify = classify.Insert(index - 2, "1");
                     classify = classify.Insert(index - 3, "{0}");
                     classify = classify.Insert(index - 4, "11");
                 }
