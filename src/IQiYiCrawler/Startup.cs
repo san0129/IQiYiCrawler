8fffc0ce62ae1c47f9a555f4749737b6432e6c69
未来 <kuangsan666@outlook.com>
2018-03-19 16:44:33 +0800

    跨域

--- src/IQiYiCrawler/Startup.cs
+++ src/IQiYiCrawler/Startup.cs
@@ -24,6 +24,7 @@ namespace IQiYiCrawler
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
