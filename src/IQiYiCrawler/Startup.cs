0c027b01c71114e3d0f02ce46dd69c5244404012
未来 <kuangsan666@outlook.com>
2018-03-19 17:08:55 +0800

    跨域代码

--- src/IQiYiCrawler/Startup.cs
+++ src/IQiYiCrawler/Startup.cs
@@ -23,8 +23,8 @@ namespace IQiYiCrawler
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             services.AddCors();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
