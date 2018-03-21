c19fbfb61feb50df9b9b37e536f73784dad1046c
未来 <kuangsan666@outlook.com>
2018-03-21 11:01:56 +0800

    cache

---
+++ src/IQiYiCrawler/Caching/Extensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IQiYiCrawler.Caching
+{
+    public static class CacheExtensions
+    {
+        private static int DefaultCacheTimeMinutes { get { return 60; } }
+
+        public static T Get<T>(this ICacheManager cacheManager, string key, Func<T> acquire)
+        {
+            //use default cache time
+            return Get(cacheManager, key, DefaultCacheTimeMinutes, acquire);
+        }
+
+        private static T Get<T>(ICacheManager cacheManager, string key, int cacheTime, Func<T> acquire)
+        {
+            if (cacheManager.IsSet(key))
+                return cacheManager.Get<T>(key);
+
+            var result = acquire();
+
+            if (cacheTime > 0)
+                cacheManager.Set(key, result, cacheTime);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Removes items by pattern
+        /// </summary>
+        /// <param name="cacheManager">Cache manager</param>
+        /// <param name="pattern">Pattern</param>
+        /// <param name="keys">All keys in the cache</param>
+        public static void RemoveByPattern(this ICacheManager cacheManager, string pattern, IEnumerable<string> keys)
+        {
+            //get cache keys that matches pattern
+            var regex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var matchesKeys = keys.Where(key => regex.IsMatch(key)).ToList();
+
+            //remove matching values
+            matchesKeys.ForEach(cacheManager.Remove);
+        }
+    }
+}
---
+++ src/IQiYiCrawler/Caching/PerRequestCacheManager.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IQiYiCrawler.Caching
+{
+    public class PerRequestCacheManager : ICacheManager
+    {
+
+        #region Fields
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        #endregion
+
+        #region Ctor
+        public PerRequestCacheManager(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        #endregion
+
+        #region Utilities
+
+        /// <summary>
+        /// Gets a key/value collection that can be used to share data within the scope of this request 
+        /// </summary>
+        protected virtual IDictionary<object, object> GetItems()
+        {
+            return _httpContextAccessor.HttpContext?.Items;
+        }
+
+        #endregion
+
+        #region Methods
+        public T Get<T>(string key)
+        {
+            var items = GetItems();
+            if (items == null)
+                return default(T);
+            return (T)items[key];
+        }
+        public void Set(string key, object data, int cacheTime)
+        {
+            var items = GetItems();
+            if (items == null)
+                return;
+
+            if (data != null)
+                items[key] = data;
+        }
+
+        public bool IsSet(string key)
+        {
+            var items = GetItems();
+            return items?[key] != null;
+        }
+
+        public void Remove(string key)
+        {
+            var items = GetItems();
+            items?.Remove(key);
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            var items = GetItems();
+            if (items == null)
+                return;
+            this.RemoveByPattern(pattern, items.Keys.Select(p => p.ToString()));
+        }
+
+        public void Clear()
+        {
+            var items = GetItems();
+
+            items?.Clear();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+
+}
