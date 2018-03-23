afd693dd79be77586df8202afcbd94fdf49a09c7
未来 <kuangsan666@outlook.com>
2018-03-23 14:29:54 +0800

    addsession

--- src/IQiYiCrawler/Startup.cs
+++ src/IQiYiCrawler/Startup.cs
@@ -38,6 +38,7 @@ namespace IQiYiCrawler
                 });
             });
             services.AddMvc();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +57,7 @@ namespace IQiYiCrawler
                     name: "default",
                     template: "api/{controller=movies}/{action=get}/{id?}");
             });
+            app.UseSession();
             app.UseCors("any");
         }
     }
