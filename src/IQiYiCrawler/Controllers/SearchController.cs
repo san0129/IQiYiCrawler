080cc09f945e1e96b540b0e3c0f29369167b4fd7
未来 <kuangsan666@outlook.com>
2018-03-27 14:07:49 +0800

    跨域

--- src/IQiYiCrawler/Controllers/SearchController.cs
+++ src/IQiYiCrawler/Controllers/SearchController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IQiYiCrawler.Models;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace IQiYiCrawler.Controllers
 {
-    [Route("api/[controller]")]
+    [EnableCors("any")]
     public class SearchController : Controller
     {
         // GET: api/<controller>
@@ -19,31 +20,6 @@ namespace IQiYiCrawler.Controllers
             //http://so.iqiyi.com/so/q_你的名字
             string queryStr = $"http://so.iqiyi.com/so/q_{query}";
             return QuerySearch.Crawler(queryStr);
-        }
-
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
